6ebbf480bcd1453798048bf618dc053576123788
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2023-07-28 14:22:51 +0200

    improving html/markdown layout

--- scripts/createAllMDFromYaml.sh
+++ scripts/createAllMDFromYaml.sh
@@ -11,7 +11,7 @@ ct=$(yq "(. | length) - 1" ${input}/modules.yaml)
 for i in $(seq 0 $ct); do
     export YQ_MODULE_IDX=$i
     echo -n "id: $i; "
-    name=$(yq -r '.[env.YQ_MODULE_IDX | tonumber].name' ${input}/modules.yaml)
+    name=$(yq -r 'sort_by(.name) | .[env.YQ_MODULE_IDX | tonumber].name' ${input}/modules.yaml)
     echo ${name}
     ./scripts/createMDFromYaml.yq ${input}/modules.yaml > "${output}/modules/${name}.md"
 done
